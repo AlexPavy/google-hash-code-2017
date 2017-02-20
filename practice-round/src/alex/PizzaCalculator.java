c0e97e6d757093aa52a988f4ed2a35b1f1105612
Alex Pavy <alex.g.pavy@gmail.com>
2017-02-20 21:39:49 +0100

    practice round structure

---
+++ practice-round/src/alex/PizzaCalculator.java
@@ -0,0 +1,15 @@
+package alex;
+
+public class PizzaCalculator {
+
+    public int R;
+    public int C;
+    public int L;
+    public int H;
+    public boolean[][] initialPizza; // 0 = T; 1 = M
+
+    void cutParts() {
+
+    }
+
+}
