44fecf6d02247d2a51074b737a9e4a004e89c56c
Alex Pavy <alex.g.pavy@gmail.com>
2017-02-23 18:26:27 +0100

    qualification round

--- practice-round/src/alex/PizzaCalculator.java
+++ practice-round/src/alex/PizzaCalculator.java
@@ -2,8 +2,47 @@ package alex;
 
 public class PizzaCalculator {
 
-    void cutParts(PizzaData data) {
+    void cutParts() {
 
+    }
+
+    /**
+     * Limited by int i1, int i2, int j1, int j2
+     */
+    int enumeratePizzas(
+            PizzaData data, int i1, int i2, int j1, int j2) {
+        int n = 0;
+        for (int i=i1;i<=i2;i++) {
+            for (int j=j1;j<j2;j++) {
+                if (data.initialPizza[i][j] != -1) {
+                    n += enumerateStartingOn(data, i, i2, j, j2, 0, 0);
+                }
+            }
+        }
+    }
+
+    private int enumerateStartingOn(
+            PizzaData data, int i1, int i2, int j1, int j2,
+            int mush, int toma) {
+
+        PizzaData copy = new PizzaData(data);
+        int n = 0;
+        for (int i=i1;i<=i2;i++) {
+            for (int j=j1;j<j2;j++) {
+                if (copy.initialPizza[i][j] == 0) {
+                    copy.initialPizza[i][j] = -1;
+                    toma++;
+                    if (toma + mush <= data.H) {
+                        if (toma >= copy.L && mush >= copy.L) {
+                            n++;
+                        }
+                    }
+                    break;
+
+                }
+            }
+        }
+        return 0;
     }
 
 }
--- practice-round/src/alex/PizzaData.java
+++ practice-round/src/alex/PizzaData.java
@@ -8,4 +8,14 @@ public class PizzaData {
     public int H;
     public int[][] initialPizza; // 0 = T; 1 = M
 
+    public PizzaData() {}
+
+    public PizzaData(final PizzaData pizzaData) {
+        this.R = pizzaData.R;
+        this.C = pizzaData.C;
+        this.L = pizzaData.L;
+        this.H = pizzaData.H;
+        this.initialPizza = pizzaData.initialPizza.clone();
+    }
+
 }
